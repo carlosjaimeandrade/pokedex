d5439a8eb228d588c5b1169b180a1cc7de850b00
Carlos Jaime <jaime_andrek@hotmail.com>
2022-08-19 22:24:30 -0300

    listando os pokemon

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
   <div id="app">
-    
+    <div v-for="(poke, index) in pokemons" :key="index">
+      <Pokemon :name="poke.name" :url="poke.url" :num="index + 1" />
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Pokemon from './components/Pokemon'
 export default {
   name: 'App',
+  data() {
+    return {
+      pokemons: []
+    }
+  },
+  created: function (){
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=150&offset=0').then(res=>{
+    this.pokemons = res.data.results
+    })
+  },
+  components:{
+    Pokemon
+  }
 }
 </script>
 
