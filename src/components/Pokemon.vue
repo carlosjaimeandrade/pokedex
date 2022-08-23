169123ad09161967c70707183edddc641d735fca
Carlos Jaime <jaime_andrek@hotmail.com>
2022-08-22 21:25:41 -0300

    adicionando card

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,42 +1,58 @@
 <template>
-    <div>
-        <h1>{{num}} {{upper(name)}}</h1>
-        <small>{{url}}</small>
+  <div>
+    <div class="card">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left">
+            <figure class="image is-48x48">
+              <img
+                :src="pokemon.front"
+                alt="Placeholder image"
+              />
+            </figure>
+          </div>
+          <div class="media-content">
+            <p class="title is-4">{{ upper(name) }}</p>
+            <p class="subtitle is-6">{{ pokemon.type }}</p>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    data() {
-        return {
-            pokemon: {}
-        }
+  data() {
+    return {
+      pokemon: {},
+    };
+  },
+  props: {
+    num: Number,
+    name: String,
+    url: String,
+  },
+  methods: {
+    upper(value) {
+      var newName = value[0].toUpperCase() + value.slice(1);
+      return newName;
     },
-    props: {
-        num: Number,
-        name: String,
-        url: String
-    },
-    methods: {
-        upper(value){
-            var newName = value[0].toUpperCase() + value.slice(1)
-            return newName
-        }
-    },
-    created: function(){
-        axios.get(this.url).then(response =>{
-            this.pokemon.type = response.data.types[0].type.name
-            this.pokemon.front = response.data.sprites.front_default
-            this.pokemon.back = response.data.sprites.back_default
-        }
-    )
-}
-  
-}   
+  },
+  created: function () {
+    axios.get(this.url).then((response) => {
+      this.pokemon.type = response.data.types[0].type.name;
+      this.pokemon.front = response.data.sprites.front_default;
+      this.pokemon.back = response.data.sprites.back_default;
+    });
+  },
+};
 </script>
 
 <style>
-
+.card{
+    margin: 10px
+}
 </style>
