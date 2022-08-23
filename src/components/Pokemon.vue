fd8fc10ad0340f89d5da8b7ed2aa5970e8f2b5fe
Carlos Jaime <jaime_andrek@hotmail.com>
2022-08-22 21:13:48 -0300

    consumindo as url da api REST do hatoes

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -6,8 +6,14 @@
 </template>
 
 <script>
+import axios from 'axios';
 
 export default {
+    data() {
+        return {
+            pokemon: {}
+        }
+    },
     props: {
         num: Number,
         name: String,
@@ -18,7 +24,16 @@ export default {
             var newName = value[0].toUpperCase() + value.slice(1)
             return newName
         }
-    }
+    },
+    created: function(){
+        axios.get(this.url).then(response =>{
+            this.pokemon.type = response.data.types[0].type.name
+            this.pokemon.front = response.data.sprites.front_default
+            this.pokemon.back = response.data.sprites.back_default
+        }
+    )
+}
+  
 }   
 </script>
 
