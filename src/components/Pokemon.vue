1a6204ff0836b9c65063fc87b71ecf2e5e208536
Carlos Jaime <jaime_andrek@hotmail.com>
2022-08-23 21:36:18 -0300

    Alternando entre frente e tras

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -4,17 +4,15 @@
       <div class="card-content">
         <div class="media">
           <div class="media-left">
-            <figure class="image is-48x48">
-              <img
-                :src="pokemon.front"
-                alt="Placeholder image"
-              />
+            <figure class="image is-90x90">
+              <img :src="currentImg" alt="Placeholder image" />
             </figure>
           </div>
           <div class="media-content">
             <p class="title is-4">{{ upper(name) }}</p>
             <p class="subtitle is-6">{{ pokemon.type }}</p>
           </div>
+          <button @click="mudarSprit()" class="button is-light">{{girar}}</button>
         </div>
       </div>
     </div>
@@ -27,6 +25,9 @@ import axios from "axios";
 export default {
   data() {
     return {
+      girar: "Ver atrás",
+      isFront: true,
+      currentImg: "",
       pokemon: {},
     };
   },
@@ -40,19 +41,31 @@ export default {
       var newName = value[0].toUpperCase() + value.slice(1);
       return newName;
     },
+    mudarSprit: function () {
+      if (this.isFront) {
+        this.isFront = false;
+        this.currentImg = this.pokemon.back;
+        this.girar = "Ver frete"
+      }else{
+        this.isFront = true;
+        this.currentImg = this.pokemon.front;
+        this.girar = "Ver atrás"
+      }
+    },
   },
   created: function () {
     axios.get(this.url).then((response) => {
       this.pokemon.type = response.data.types[0].type.name;
       this.pokemon.front = response.data.sprites.front_default;
       this.pokemon.back = response.data.sprites.back_default;
+      this.currentImg = this.pokemon.front;
     });
   },
 };
 </script>
 
 <style>
-.card{
-    margin: 10px
+.card {
+  margin: 10px;
 }
 </style>
